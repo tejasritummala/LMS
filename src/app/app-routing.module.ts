bcabf323ba9a138d119ac2a4067d8e031942457e
tejasritummala <tejasritummala@gmail.com>
2023-03-10 21:30:07 -0600

    pushing local changes to git

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { Routes, RouterModule } from '@angular/router';
+import { CalendarComponent } from './calendar/calendar.component';
+import { ApplyLeaveComponent } from './apply-leave/apply-leave.component';
+import { RejectComponent } from './approve/reject/reject.component';
+import { EmpAttComponent } from './emp-att/emp-att.component';
+import { ProfileComponent } from './profile/profile.component';
+const routes: Routes = [{path:"",component:CalendarComponent},{path:"applyLeave",component:ApplyLeaveComponent},
+{path:"approveReject",component:RejectComponent},{path:"empAtt",component:EmpAttComponent},{path:"profile",component:ProfileComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,57 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { HttpClientModule }    from '@angular/common/http';
+import { FormsModule }    from '@angular/forms';
+import { FullCalendarModule } from '@fullcalendar/angular'; 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { ApplyLeaveComponent } from './apply-leave/apply-leave.component';
+import { RejectComponent } from './approve/reject/reject.component';
+import { EmpAttComponent } from './emp-att/emp-att.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTableModule} from "@angular/material/table";
+import {MatDialogModule} from "@angular/material/dialog";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import { ProfileComponent } from './profile/profile.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import {NgxMaterialTimepickerModule} from 'ngx-material-timepicker';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { DxSchedulerModule } from 'devextreme-angular';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarComponent,
+    ApplyLeaveComponent,
+    RejectComponent,
+    EmpAttComponent,
+    ProfileComponent,
+    TopBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FullCalendarModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatDialogModule,
+    MatToolbarModule,
+    HttpClientModule,
+    FormsModule,
+    NgxMaterialTimepickerModule,
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory
+    }),
+    DxSchedulerModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ]
 })
 export class AppModule { }
